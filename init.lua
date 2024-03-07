c9ef00a789d1cf87e9a2d848956b3d411fcdcdb6
PixelMaker06 <twoc0bb@gmail.com>
2024-03-07 18:14:09 +1030

    added some stuff, mainly autocomplete with nvim-cmp

--- init.lua
+++ init.lua
@@ -36,7 +36,18 @@ require("lazy").setup({
 	{"neovim/nvim-lspconfig"},
 
     -- Autocomplete
-    --{"https://github.com/hrsh7th/nvim-cmp"},
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lua",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "saadparwaiz1/cmp_luasnip",
+            "L3MON4D3/LuaSnip",
+        },
+    },
 })
 
 require("telescope").setup {
@@ -55,7 +66,7 @@ vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 
 require('lualine').setup {
     options = {
-        theme = 'gruvbox',
+        theme = 'catppuccin',
     },
 }
 
@@ -108,10 +119,51 @@ lspconfig.slint_lsp.setup {
 }
 
 -- setup autocomplete
---local cmp = require("cmp")
+local cmp = require("cmp")
+
+cmp.setup({
+
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+        end,
+    },
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+        --documentation = false;
+
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        --["<CR>"] = cmp.mapping.open_docs()
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "luasnip" }, -- For luasnip users.
+        -- { name = "orgmode" },
+    }, {
+        { name = "buffer" },
+        { name = "path" },
+    }),
+})
+
+cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = "path" },
+    }, {
+        { name = "cmdline" },
+    }),
+})
 
 -- vim.o.background = "light"
-vim.cmd "colorscheme gruvbox"
+vim.cmd "colorscheme catppuccin"
 vim.cmd("set wrap!")
 
 vim.opt.tabstop = 4
